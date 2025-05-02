ffabc988f5dedfee0107d21cadd5da7a88e7dfde
HUAHUA768 <1783789220@qq.com>
2025-05-02 13:08:07 +0800

    float单精度和double双精度的区别

---
+++ 007单双精度的使用及科学计数法/FileName.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float f1 = 3.14f;//为什么要加f
+	double d1 = 3.14;
+	cout << f1 << endl;
+	cout << d1 << endl;
+	//科学计数法
+	float f2 = 3e2;   //表示3*10²
+		double d2 = 3e-2;
+	cout << f2 << endl;
+	cout << d2 << endl;
+
+
+	system("pause");
+	return 0;
+
+}
