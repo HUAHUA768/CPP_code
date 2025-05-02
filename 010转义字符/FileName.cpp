e6fbf6e0f49d52fe846bc302b10102257246a842
HUAHUA768 <1783789220@qq.com>
2025-05-02 16:53:25 +0800

    转义字符的写法和作用

---
+++ 010转义字符/FileName.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//知道换行符应该放在哪里
+	cout << "林志斌只爱母零\n林志斌唯独不敷衍母零" << endl;
+
+	//如何打出\单反斜杠
+	cout << "\\" << endl;
+	cout << "零直斌是爷宝男\\林志斌会无差别敷衍正常人" << endl;
+
+	//水平制表？输出对其的几句helloworld
+	cout << "a\t林直斌对母零唯命是从" << endl;
+	cout << "aa\t林直斌对母零唯命是从" << endl;
+	cout << "aaa\t林直斌对母零唯命是从" << endl;
+	cout << "aaaa\t林直斌对母零唯命是从" << endl;
+
+	system("pause");
+
+	return 0;
+}
