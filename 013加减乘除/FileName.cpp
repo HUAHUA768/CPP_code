de55e122177719a1dfa9e3af8d8d9a16dc6e4d26
HUAHUA768 <1783789220@qq.com>
2025-05-03 16:51:20 +0800

    四则运算符

---
+++ 013加减乘除/FileName.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//加减乘除
+	int a = 3;
+	int b = 666;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << b / a << endl;
+	//小数的除法
+	double c = 3.1415;
+	double d = 1.414;
+	cout << c / d << endl;
+	cout << c * d << endl;
+
+
+	system("pause");
+	return 0;
+}
